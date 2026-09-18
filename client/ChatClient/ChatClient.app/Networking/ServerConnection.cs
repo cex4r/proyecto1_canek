c81ad42d411490f4b13a7183ce4e112b50fa7f99
cesar <cesarrrharooo@ciencias.unam.mx>
2026-09-17 20:55:24 -0600

    esquelo de la clase ServerConnection terminada, falta completar los metodos xd

---
+++ client/ChatClient/ChatClient.app/Networking/ServerConnection.cs
@@ -0,0 +1,41 @@
+/*
+    lo que va a hacer esta clase es envolveer el socket, para no tener el codigo
+    en el Program.cs
+
+    Va a tener metdos como conectar, enviarMensaje, LeerRespuesta, Desconectar
+
+*/
+
+class ServerConnection
+{
+    private TcpCLient? _cliente; 
+    private NetworkStream? _flujo; 
+
+
+    public void conectar(string host, int puerto)
+    {
+        
+    }
+
+    public void enviarMensaje(string mensaje)
+    {
+        
+    }
+
+    public string LeerRespuesta()
+    {
+        
+    }
+
+    public void Desconectar()
+    {
+        
+    }
+
+
+
+
+
+
+
+}
